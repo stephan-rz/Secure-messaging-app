f5dabf636e2b55d299d120d516a4f87067a5ad29
Shamal Chathuranga Wijesuriya <shamalchathuranga2@gmail.com>
2024-05-20 09:47:15 +0530

    fix

--- src/lib/upstash.ts
+++ src/lib/upstash.ts
@@ -1,7 +1,7 @@
 import { Redis } from '@upstash/redis'
 
 export const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL! ?? "https://vocal-dogfish-42154.upstash.io",
+  url: "https://vocal-dogfish-42154.upstash.io",
   token: process.env.UPSTASH_REDIS_REST_TOKEN!,
 })
 
